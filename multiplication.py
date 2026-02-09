08c86ab77f36cbc46917fd8e9a391492bf32ea2a
Laviedotone <laviedotone@gmail.com>
2026-02-09 00:56:08 -0600

    multiplication function

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+def multiply():
+
+    num1 = int(input("Enter your first number to multiply: "))
+    num2 = int(input("Enter your second number to multiply: "))
+
+    result = num1 * num2
+
+    print("Your result is: " + str(result))
